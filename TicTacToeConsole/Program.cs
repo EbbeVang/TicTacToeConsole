5c985e2b60351bbfee820e31d7fbcf2deb208cce
laptop\Ebbe Vang <ev@evang.dk>
2018-11-02 13:29:55 +0100

    added console colors

--- TicTacToeConsole/Program.cs
+++ TicTacToeConsole/Program.cs
@@ -13,25 +13,31 @@ namespace TicTacToeConsole
         static void Main(string[] args)
         {
             //play game
-            
             g = new TicTacToeGame();
-            Console.WriteLine("Welcome to TicaTacToe \nplease entr your names");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(showAsciiArt());
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Welcome to TicTacToe \nplease enter your names");
             g.setup(Console.ReadLine(), Console.ReadLine());
-
+            Console.Clear();
             while (true)
             {
+                Console.WriteLine(showAsciiArt());
                 showBoard();
-                Console.WriteLine("play for helvede" + g.CurrentPlayer.Name);
+                Console.WriteLine("play " + g.CurrentPlayer.Name + "!");
                 Console.Write("row:");
                 int row = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
+                Console.WriteLine();
                 Console.Write("col:");
                 int col = Convert.ToInt32(Console.ReadKey().KeyChar.ToString());
+                Console.WriteLine();
                 if (g.playPiece(g.CurrentPlayer.Piece, row, col))
                 {
                     g.switchPlayer();
 
                 }
                 Console.Clear();
+                
             }
 
             //TestBoard();
@@ -39,19 +45,31 @@ namespace TicTacToeConsole
 
         public static void showBoard()
         {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("+---+---+---+");
             for (int i = 0; i < 3; i++)
             {
                 Console.Write("|");
                 for (int j = 0; j < 3; j++)
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" ");
                     Console.Write(g.getBoard()[i,j]);
+                    Console.Write(" ");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.Write("|");
                 }
 
                 Console.WriteLine();
+                Console.WriteLine("+---+---+---+");
             }
         }
 
+        private static string showAsciiArt()
+        {
+            return "▄▄▄█████▓ ██▓ ▄████▄  ▄▄▄█████▓ ▄▄▄       ▄████▄  ▄▄▄█████▓ ▒█████  ▓█████\r\n▓  ██▒ ▓▒▓██▒▒██▀ ▀█  ▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█  ▓  ██▒ ▓▒▒██▒  ██▒▓█   ▀\r\n▒ ▓██░ ▒░▒██▒▒▓█    ▄ ▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▒ ▓██░ ▒░▒██░  ██▒▒███\r\n░ ▓██▓ ░ ░██░▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒░ ▓██▓ ░ ▒██   ██░▒▓█  ▄\r\n ▒██▒ ░ ░██░▒ ▓███▀ ░  ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░  ▒██▒ ░ ░ ████▓▒░░▒████▒\r\n ▒ ░░   ░▓  ░ ░▒ ▒  ░  ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░  ▒ ░░   ░ ▒░▒░▒░ ░░ ▒░ ░\r\n   ░     ▒ ░  ░  ▒       ░      ▒   ▒▒ ░  ░  ▒       ░      ░ ▒ ▒░  ░ ░  ░\r\n ░       ▒ ░░          ░        ░   ▒   ░          ░      ░ ░ ░ ▒     ░\r\n         ░  ░ ░                     ░  ░░ ░                   ░ ░     ░  ░\r\n            ░                           ░";
+        }
         private static void TestBoard()
         {
             TicTacToeBoard b = new TicTacToeBoard();
@@ -61,13 +79,13 @@ namespace TicTacToeConsole
 
             for (int i = 0; i < 3; i++)
             {
+                Console.Write("|");
                 for (int j = 0; j < 3; j++)
                 {
                     Console.Write(b.Board[i, j]);
-                    Console.Write(" ");
+                    Console.Write("|");
                 }
 
-                Console.WriteLine();
             }
         }
     }
