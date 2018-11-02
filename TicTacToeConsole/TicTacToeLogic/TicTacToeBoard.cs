f6c2c4e1b1b9e9ee5b6070a5d9dc074cec53a44e
laptop\Ebbe Vang <ev@evang.dk>
2018-11-02 10:13:28 +0100

    Update TicTacToeConsole/TicTacToeLogic/TicTacToeBoard.cs

--- TicTacToeConsole/TicTacToeLogic/TicTacToeBoard.cs
+++ TicTacToeConsole/TicTacToeLogic/TicTacToeBoard.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace TicTacToeConsole
-{
+namespace TicTacToeConsole{
     class TicTacToeBoard
     {
+        private const int _boardSize = 3;
+        private char[] _pieces = { 'x', 'o' };
+        private char[,] _board = new char[_boardSize, _boardSize];
+        
+        public char[,] Board
+        {
+            get { return _board; }
+        }
+        public Boolean setPiece(char c, int row, int col)
+        {
+            if (validInput(c, row, col))
+            {
+                _board[row, col] = c;
+                return true;
+            }
+            else return false;
+
+        }
+
+        private bool validInput(char c, int row, int col)
+        {
+            if (_pieces.Contains(c))
+            {
+                if (row >= 0 && row < _boardSize)
+                {
+                    if (col >= 0 && col < _boardSize)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
